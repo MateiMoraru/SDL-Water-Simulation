a632167cb29636770fd2ed0eda25ad3441a864f9
MateiMoraru <moraru.matei24@gmail.com>
2026-01-21 21:54:01 +0200

    Added flowing water

--- SDL-Water-Simulation/main.c
+++ SDL-Water-Simulation/main.c
@@ -4,14 +4,32 @@
 #define true 1
 #define false 0
 
-#define TILE_SIZE 32
-#define GRID_WIDTH 40
-#define GRID_HEIGHT 25
+#define TILE_SIZE 8
+#define GRID_WIDTH 160
+#define GRID_HEIGHT 100
 #define WIDTH TILE_SIZE * GRID_WIDTH
 #define HEIGHT TILE_SIZE * GRID_HEIGHT
 
 #define FPS 60
 #define FPS_DELAY 1000/FPS
+
+#define EMPTY_ID 0
+#define WATER_ID 1
+#define WALL_ID -1
+
+#define WATER_MAX_ID 32
+
+#define COLOR_RED_MIN 0
+#define COLOR_GREEN_MIN 29
+#define COLOR_BLUE_MIN 77
+
+#define COLOR_RED_MAX 79
+#define COLOR_GREEN_MAX 129
+#define COLOR_BLUE_MAX 219
+
+#define DRAW_LINES false
+
+const int flow_dirs[3][2] = { {0, 1}, {-1, 0}, {1, 0} };
 
 typedef struct
 {
@@ -20,30 +38,158 @@ typedef struct
 	int button;
 } Mouse;
 
+typedef struct
+{
+	int x;
+	int y;
+} Vec2;
+
+typedef struct
+{
+	int x;
+	int y;
+	int z;
+} Vec3;
+
+int clamp(int val, int min, int max)
+{
+	if (val < min)
+		return min;
+	if (val > max)
+		return max;
+	return val;
+}
+
+void swap(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
+
+Vec3 calculate_color(int grid[GRID_HEIGHT][GRID_WIDTH], int x, int y)
+{
+	int sum = 0;
+	for (int i = y - 1; i >= 0; i--)
+	{
+		int id = grid[i][x];
+		if (id == EMPTY_ID)
+			break;
+		if (id >= WATER_ID)
+			sum++;
+	}
+
+	sum = clamp(sum, WATER_ID, WATER_MAX_ID);
+
+	float t = (float)sum / (float)WATER_MAX_ID;
+
+	int r = clamp((int)(COLOR_RED_MAX * (1.0f - t)), COLOR_RED_MIN, COLOR_RED_MAX);
+	int g = clamp((int)(COLOR_GREEN_MAX * (1.0f - t)), COLOR_GREEN_MIN, COLOR_GREEN_MAX);
+	int b = clamp((int)(COLOR_BLUE_MAX * (1.0f - t)), COLOR_BLUE_MIN, COLOR_BLUE_MAX);
+
+	Vec3 color = { r, g, b };
+
+	return color;
+}
+
+Vec2 process_water(int grid[GRID_HEIGHT][GRID_WIDTH], int x, int y)
+{
+	Vec2 newpos = { -1, -1 };
+	int nx = x + flow_dirs[0][0];
+	int ny = y + flow_dirs[0][1];
+	if (nx >= 0 && nx < GRID_WIDTH && ny >= 0 && ny < GRID_HEIGHT && grid[ny][nx] == EMPTY_ID)
+	{
+		grid[ny][nx] = grid[y][x];
+		grid[y][x] = EMPTY_ID;
+		newpos.x = nx;
+		newpos.y = ny;
+		return newpos;
+	}
+
+	int lateral[2] = { 1, 2 };
+	if (rand() % 2)
+	{
+		int tmp = lateral[0];
+		lateral[0] = lateral[1];
+		lateral[1] = tmp;
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		int idx = lateral[i];
+		nx = x + flow_dirs[idx][0];
+		ny = y + flow_dirs[idx][1];
+
+		if (nx >= 0 && nx < GRID_WIDTH && ny >= 0 && ny < GRID_HEIGHT && grid[ny][nx] == EMPTY_ID)
+		{
+			grid[ny][nx] = grid[y][x];
+			grid[y][x] = EMPTY_ID;
+			newpos.x = nx;
+			newpos.y = ny;
+			return newpos;
+		}
+	}
+
+	return newpos;
+}
+
 void draw_grid(int grid[GRID_HEIGHT][GRID_WIDTH], SDL_Renderer* renderer)
 {
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 	SDL_Rect rect;
-	rect.w = TILE_SIZE;
-	rect.h = TILE_SIZE;
-	for (int y = 0; y < GRID_HEIGHT; y++)
+	rect.w = TILE_SIZE - 1;
+	rect.h = TILE_SIZE - 1;
+
+	for (int y = GRID_HEIGHT - 1; y >= 0; y--)
 	{
 		for (int x = 0; x < GRID_WIDTH; x++)
 		{
-			SDL_RenderDrawLine(renderer, x * TILE_SIZE, y * TILE_SIZE, x * TILE_SIZE, y * TILE_SIZE + TILE_SIZE);
-			SDL_RenderDrawLine(renderer, x * TILE_SIZE, y * TILE_SIZE, x * TILE_SIZE + TILE_SIZE, y * TILE_SIZE);
-
-			if (grid[y][x] == 1)
+			if (DRAW_LINES)
 			{
-				rect.x = x * TILE_SIZE;
-				rect.y = y * TILE_SIZE;
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+				SDL_RenderDrawLine(renderer, x * TILE_SIZE, y * TILE_SIZE, x * TILE_SIZE, y * TILE_SIZE + TILE_SIZE);
+				SDL_RenderDrawLine(renderer, x * TILE_SIZE, y * TILE_SIZE, x * TILE_SIZE + TILE_SIZE, y * TILE_SIZE);
+			}
+
+			int id = grid[y][x];
+
+			if (id == -1)
+			{
+				rect.x = x * TILE_SIZE + 1;
+				rect.y = y * TILE_SIZE + 1;
+
+				SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 				SDL_RenderFillRect(renderer, &rect);
+			}
+			else if (id >= WATER_ID)
+			{
+				rect.x = x * TILE_SIZE + 1;
+				rect.y = y * TILE_SIZE + 1;
+
+				Vec3 color = calculate_color(grid, x, y);
+				
+				SDL_SetRenderDrawColor(renderer, color.x, color.y, color.z, 255);
+				
+				SDL_RenderFillRect(renderer, &rect);
+
 			}
 
 		}
 	}
 	SDL_RenderDrawLine(renderer, 0, HEIGHT - 1, WIDTH, HEIGHT - 1);
 	SDL_RenderDrawLine(renderer, WIDTH - 1, 0, WIDTH - 1, HEIGHT);
+}
+
+void update_grid(int grid[GRID_HEIGHT][GRID_WIDTH])
+{
+	for (int y = GRID_HEIGHT - 1; y >= 0; y--)
+	{
+		for (int x = 0; x < GRID_WIDTH; x++)
+		{
+			int id = grid[y][x];
+			if (id >= WATER_ID)
+				process_water(grid, x, y);
+		}
+	}
 }
 
 void handle_mouse(Mouse* mouse, int grid[GRID_HEIGHT][GRID_WIDTH])
@@ -53,7 +199,14 @@ void handle_mouse(Mouse* mouse, int grid[GRID_HEIGHT][GRID_WIDTH])
 		int x = mouse->x / TILE_SIZE;
 		int y = mouse->y / TILE_SIZE;
 
-		grid[y][x] = 1;
+		grid[y][x] = WALL_ID;
+	}
+	else if (mouse->button == SDL_BUTTON_RIGHT)
+	{
+		int x = mouse->x / TILE_SIZE;
+		int y = mouse->y / TILE_SIZE;
+
+		grid[y][x] = WATER_ID;
 	}
 }
 
@@ -81,18 +234,31 @@ int main()
 					running = false;
 					break;
 
-				case SDL_MOUSEBUTTONDOWN:
+				/*case SDL_MOUSEBUTTONDOWN:
 					mouse.button = event.button.button;
 					mouse.x = event.button.x;
 					mouse.y = event.button.y;
-					break;
+					break;*/
 			}
+		}
+
+		Uint32 buttons = SDL_GetMouseState(&mouse.x, &mouse.y);
+
+		if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT))
+		{
+			mouse.button = SDL_BUTTON_LEFT;
+		}
+
+		if (buttons & SDL_BUTTON(SDL_BUTTON_RIGHT))
+		{
+			mouse.button = SDL_BUTTON_RIGHT;
 		}
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
 		draw_grid(grid, renderer);
+		update_grid(grid);
 
 		handle_mouse(&mouse, grid);
 
